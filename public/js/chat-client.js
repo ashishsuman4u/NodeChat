caefb22282ff06818db5588cca50e67fec60dae9
Ashish Suman <ashishsuman4u@gmail.com>
2016-05-18 07:55:13 +0530

    Initial commit

---
+++ public/js/chat-client.js
@@ -0,0 +1,78 @@
+/**
+ * Created by Ashish Astha on 5/15/2016.
+ */
+(function () {
+    var socket = null;
+    var chat = (function(){
+        var rooms = [];
+        var $btnStatus = $("#status");
+        var $roomName = $("#roomname");
+        var $nickname = $("#nickname");
+        var $chatWindow = $("#chatwindow");
+        var $roomWindow = $("#roomwindow");
+        var $chatText = $("#chattext");
+        var $btnSubmit = $("#btnSubmit");
+
+        $btnStatus.on("click", changeStatus);
+        $btnSubmit.on("click", sendChat);
+        $roomWindow.delegate( "a", "click", changeRoom);
+
+        function setSocketConnection(socket) {
+            socket.on("setnickname", function (data) {
+                $nickname.text("Hi " + data.nickname);
+            });
+            socket.on("currentroom", function (data) {
+                $roomName.text("Current Room - " + data.room);
+            });
+            socket.on("joinedroom", function (data) {
+                $chatWindow.append(data.message + "<br>");
+            });
+            socket.on("changenickname", function (data) {
+                $chatWindow.append(data.msg + "<br>");
+            });
+            socket.on("showchat", function (data) {
+                $chatWindow.append(data.chat + "<br>");
+            });
+            socket.on("rooms", function (data) {
+                $roomWindow.html("");
+                for(var index in data.rooms){
+                    $roomWindow.append("<a>"+ data.rooms[index] +"</a>" + "<br>");
+                }
+            });
+        }
+
+        function sendChat() {
+            socket.emit("newchat", {chat: $chatText.val()});
+            $chatText.val("");
+        }
+
+        function changeRoom(e) {
+            socket.emit("newchat", {chat: "/r " + e.target.innerText});
+            $chatWindow.html("");
+        }
+
+        function changeStatus() {
+            if($btnStatus.val() === "Login"){
+                socket = io('http://localhost:3000',{'forceNew':true });
+                $btnStatus.val("Logout");
+                setSocketConnection(socket);
+            }
+            else{
+                socket.disconnect();
+                $btnStatus.val("Login");
+                resetWindow();
+            }
+        }
+
+        function resetWindow() {
+            $roomWindow.html("");
+            $nickname.text("");
+            $roomName.text("");
+            $chatWindow.html("");
+        }
+        return {
+            changeStatus: changeStatus,
+            changeRoom: changeRoom
+        }
+    })();
+})();
---
+++ server.js
@@ -0,0 +1,93 @@
+(function () {
+    var node_static = require("node-static");
+    var http = require("http");
+    var io = require("socket.io");
+    var port = 3000;
+    var currentRoom = {};
+    var nickName = [];
+    var user = 10;
+    var rooms = ["Lobby"];
+
+    var fileServer = new node_static.Server('./public');
+
+    var server = http.createServer(function (req, res) {
+        fileServer.serve(req,res);
+    }).listen(port, function() {
+        console.log("Server listening on port " + port);
+    });
+
+    var socketio = new io(server);
+
+    socketio.on("connection", function (socket) {
+        console.log("connected - " + socket.id);
+        assignNickname(socket, "Guest" + user++);
+        joinRoom(socket, "Lobby");
+        showRooms();
+        socket.on("newchat",function (data) {
+            newChat(socket, data.chat);
+        });
+        socket.on("disconnect",function () {
+            console.log("disconnected");
+            notifyOldRoom(socket, "")
+        });
+    });
+
+    function newChat(socket, chatText) {
+        chatText = chatText.trim();
+        if(chatText.substr(0,2) === "/r"){
+            joinRoom(socket, chatText.substr(2,chatText.length-1).trim());
+            showRooms();
+        }
+        else if(chatText.substr(0,2) === "/n"){
+            changeNickname(socket, chatText.substr(2,chatText.length-1).trim());
+        }
+        else{
+            socketio.in(currentRoom[socket.id]).emit("showchat", {chat:nickName[socket.id] + " : "+ chatText});
+        }
+    }
+
+    function changeNickname(socket, nickname) {
+        socket.broadcast.to(currentRoom[socket.id]).emit("changenickname", {msg: nickName[socket.id] + " is now " + nickname});
+        assignNickname(socket, nickname);
+    }
+
+    function assignNickname(socket, nickname) {
+        socket.nickName = nickname;
+        nickName[socket.id] = nickname;
+        socket.emit("setnickname", {nickname: nickname});
+    }
+
+    function joinRoom(socket, room) {
+        notifyOldRoom(socket, room);
+        socket.join(room);
+        currentRoom[socket.id] = room;
+        socket.emit("currentroom",{room : toTitleCase(room)});
+        socket.broadcast.to(room).emit("joinedroom", {message : nickName[socket.id] + "has joined the "+ room});
+    }
+
+    function notifyOldRoom(socket, room) {
+        if(room === "")
+        {
+            joinRoom(socket, "Lobby");
+            return;
+        }
+        if(room !== "Lobby"){
+            var oldRoom = currentRoom[socket.id];
+            socket.broadcast.to(oldRoom).emit("joinedroom", {message : nickName[socket.id] + " has left the " + oldRoom});
+            socket.leave(oldRoom);
+            if(rooms.indexOf(toTitleCase(room)) === -1) {
+                rooms.push(toTitleCase(room));
+                showRooms();
+            }
+        }
+    }
+
+    function showRooms() {
+        socketio.emit("rooms", {rooms: rooms});
+    }
+
+    function toTitleCase(str)
+    {
+        return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
+})();
